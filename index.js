0640a8910e9b27bf5345644e5d816e283c325407
Gabriel Bourgeois <gabriel.bourgeoisv4si@gmail.com>
2022-01-13 23:06:20 -0500

    Add support for the removeKeys input parameter

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const core = require('@actions/core')
-const fs = require('fs')
+const fs = require("fs")
 
-try {
-  const file = core.getInput('file')
-  const space = Number(core.getInput('space'))
+const file = process.env.INPUT_FILE
+const space = Number(process.env.INPUT_SPACE)
+const removeKeys = process.env.INPUT_REMOVEKEYS
+const original = fs.readFileSync(file, { encoding: "utf8" })
+const data = JSON.parse(original)
 
-  const original = fs.readFileSync(file, { encoding: 'utf8' })
-  const formatted = JSON.stringify(JSON.parse(original), null, space)
+removeKeys
+  .split(",")
+  .map((key) => key.trim())
+  .filter((key) => key)
+  .forEach((key) => {
+    delete data[key]
+  })
 
-  fs.writeFileSync(file, formatted)
-} catch (error) {
-  core.setFailed(error)
-}
+const formatted = JSON.stringify(data, null, space)
+
+fs.writeFileSync(file, formatted)
